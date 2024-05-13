3c88a860569d60cb84f26f22a6f93de6ef3d932b
harsh02it <harshitsavani@gmail.com>
2024-05-13 19:04:45 -0400

    Fixed the banner of the site

--- scripts/text-scrolling.js
+++ scripts/text-scrolling.js
@@ -1,5 +1,44 @@
-const texts = ["React.js Developer", "Photographer", "Anime Fan"];
+// const texts = ["React.js Developer", "Photographer", "Anime Fan"];
 
-const textSelection = document.querySelector(".banner-subtitle");
+// const textSelection = document.querySelector(".banner-subtitle");
 
-textSelection.textContent = texts[0];
+// textSelection.textContent = texts[0];
+
+document.addEventListener("DOMContentLoaded", function () {
+  var phrases = [
+    "React.js Developer",
+    "Java Developer",
+    "Gym Addict",
+    "Anime Fan",
+  ];
+  var index = 0;
+  var charIndex = 0;
+  var typingSpeed = 100; // Adjust typing speed as needed
+  var delayBeforeNextPhrase = 2000; // Adjust delay before next phrase as needed
+
+  var bannerSubtitle = document.getElementById("banner-subtitle");
+
+  function typePhrase() {
+    if (charIndex < phrases[index].length) {
+      bannerSubtitle.textContent += phrases[index].charAt(charIndex);
+      charIndex++;
+      setTimeout(typePhrase, typingSpeed);
+    } else {
+      setTimeout(erasePhrase, delayBeforeNextPhrase);
+    }
+  }
+
+  function erasePhrase() {
+    if (bannerSubtitle.textContent.length > 0) {
+      bannerSubtitle.textContent = bannerSubtitle.textContent.slice(0, -1);
+      setTimeout(erasePhrase, typingSpeed / 2);
+    } else {
+      index = (index + 1) % phrases.length;
+      charIndex = 0;
+      setTimeout(typePhrase, typingSpeed);
+    }
+  }
+
+  // Start typing the first phrase
+  typePhrase();
+});
